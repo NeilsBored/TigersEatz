e3b292a4ce87ef1e14bb122ff0310086a6e93e01
Shane John <144292740+NeilsBored@users.noreply.github.com>
2025-03-09 16:17:02 -0400

    Initial Commit

---
+++ TigerEatsTests/TigerEatsTests.swift
@@ -0,0 +1,17 @@
+//
+//  TigerEatsTests.swift
+//  TigerEatsTests
+//
+//  Created by Shane John on 3/9/25.
+//
+
+import Testing
+@testable import TigerEats
+
+struct TigerEatsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
